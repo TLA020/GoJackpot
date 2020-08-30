666fa725cc44a3482d9c84a941149f22d828a67b
Tobias <t.lie-atjam@effytool.nl>
2020-08-30 02:30:28 +0200

    use wss not ws

--- frontend/src/setup/websockets.js
+++ frontend/src/setup/websockets.js
@@ -3,8 +3,13 @@ import VueNativeSock from "vue-native-websocket";
 
 export default {
   install: Vue => {
-    const socketUrl = process.env.VUE_APP_SOCKET_URL || `ws://${location.hostname}:${location.port}/ws`;
+    let socketUrl = "";
 
+    if (!location.hostname.includes("localhost")) {
+       socketUrl = process.env.VUE_APP_SOCKET_URL || `wss://${location.hostname}:${location.port}/ws`;
+    } else {
+       socketUrl = process.env.VUE_APP_SOCKET_URL || `ws://${location.hostname}:${location.port}/ws`;
+    }
     Vue.use(VueNativeSock, `${socketUrl}`, {
       // store: store,
       format: "json"
