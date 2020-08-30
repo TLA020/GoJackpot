c234f92f5c113af9e022b1c347721566b021f7a4
Tobias <t.lie-atjam@effytool.nl>
2020-08-30 17:06:34 +0200

    change url

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import router from "@/router";
-const API_URL = (process.env.VUE_APP_API_BASE_URL || "") + "/accounts";
+const API_URL = (process.env.VUE_APP_API_BASE_URL || "/api/v1") + "/accounts";
 
 export default {
   login({commit}, { email, password }) {
