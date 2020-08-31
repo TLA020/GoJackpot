4f9e7eeb74df7a6cb9c91d4478e1661f94260e70
Tobias <t.lie-atjam@effytool.nl>
2020-08-31 09:38:15 +0200

    add error handling and loader to auth

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -4,25 +4,36 @@ const API_URL = (process.env.VUE_APP_API_BASE_URL || "/api/v1") + "/accounts";
 
 export default {
   login({commit}, { email, password }) {
-      axios.post(`${API_URL}/login`, {email, password})
+    commit("SET_LOADING", true);
+    axios.post(`${API_URL}/login`, {email, password})
         .then(({ data }) => {
-          commit("SET_USER", data)
+          commit("SET_USER", data);
+          commit("SET_ERROR", null);
           router.push("/");
         })
         .catch(err => {
+          commit("SET_ERROR", "Invalid credentials, try again");
           console.log(err)
+        })
+        .finally(() => {
+          commit("SET_LOADING", false);
         })
   },
 
   register({commit}, { email, password }) {
+    commit("SET_LOADING", true);
       axios.post(`${API_URL}/register`, {email, password})
         .then(({data}) => {
-          commit("SET_USER", data)
+          commit("SET_USER", data);
+          commit("SET_ERROR", null);
           router.push("/")
         })
         .catch(err => {
+          commit("SET_ERROR", "email already in use, consider another");
           console.log(err)
-        })
+        }).finally(() => {
+        commit("SET_LOADING", false);
+      })
   }
 };
 
